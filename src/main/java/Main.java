b26a1f198b2acd683a141a302e9175742bcb20e3
Archer6621 <shaad1@live.nl>
2016-03-30 09:06:13 +0200

    create template

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+
+/**
+ * Created by Archer on 30-Mar-16.
+ */
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+}
